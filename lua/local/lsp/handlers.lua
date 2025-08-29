fc2dd20bc349fc292f4ef8f9ad34afc3312c7a93
mosukiton <harmohan.ss@live.co.uk>
2025-08-29 02:42:39 +0100

    fixed diagnostics deprecated sign define error

--- lua/local/lsp/handlers.lua
+++ lua/local/lsp/handlers.lua
@@ -10,7 +10,7 @@ M.setup = function()
     }
 
     for _, sign in ipairs(signs) do
-        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+        -- vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
     end
 
     local config = {
@@ -18,7 +18,18 @@ M.setup = function()
         virtual_text = false,
         -- show signs
         signs = {
-            active = signs,
+            text = {
+                [vim.diagnostic.severity.ERROR] = "",
+                [vim.diagnostic.severity.WARN] = "",
+                [vim.diagnostic.severity.HINT] = "",
+                [vim.diagnostic.severity.INFO] = ""
+            },
+            linehl = {
+
+            },
+            numhl = {
+
+            }
         },
         update_in_insert = true,
         underline = true,
